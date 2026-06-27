8887de589f6a2356a24b2cec6402d89ee7ace7de
Priyanshu Bokde <priyanshubokde08@gmail.com>
2026-06-27 11:53:21 +0530

    Time: 832 ms (7.45%) | Memory: 49.6 MB (36.79%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,28 +1,50 @@
 class Solution {
     public String sortVowels(String s) {
-        int n = s.length();
-    Stack<Character> st = new Stack<>();
-    Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u','A','E','I','O','U'));
+        int[] lowercase = new int[26];
+        int[] uppercase = new int[26];
 
-    ArrayList<Character> list = new ArrayList<>();
-    for(char ch : s.toCharArray()){
-        if(set.contains(ch)) list.add(ch);
-    }
-    Collections.sort(list);
-    int k = 0;
-    for(int i = 0; i < n; i++){
-        char ch = s.charAt(i);
-        if(!set.contains(ch)){
-            st.push(ch);
-        }else{
-            st.push(list.get(k));
-            k++;
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                lowercase[arr[i] - 'a']++;
+                arr[i] = '*';
+            }
+            else if (arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U') {
+                uppercase[arr[i] - 'A']++;
+                arr[i] = '*';
+            }
         }
-    }
-    StringBuilder sb = new StringBuilder();
-    while (!st.isEmpty()) {
-        sb.append(st.pop());
-    }
-    return sb.reverse().toString();
+
+        String ans = "";
+
+        for (int i = 0; i < 26; i++) {
+            while (uppercase[i] > 0) {
+                char c = (char)('A' + i);
+                ans = ans + c;
+                uppercase[i]--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (lowercase[i] > 0) {
+                char c = (char)('a' + i);
+                ans = ans + c;
+                lowercase[i]--;
+            }
+        }
+
+        int first = 0;
+        int second = 0;
+
+        while (first < arr.length) {
+            if (arr[first] == '*') {
+                arr[first] = ans.charAt(second);
+                second++;
+            }
+            first++;
+        }
+
+        return new String(arr);
     }
 }
