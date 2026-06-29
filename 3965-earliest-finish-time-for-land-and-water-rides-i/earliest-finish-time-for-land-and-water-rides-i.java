b60294f56d2300e8861e987f4c3976cfa857d253
Priyanshu Bokde <priyanshubokde08@gmail.com>
2026-06-29 11:53:49 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (40.51%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int earliestFinishTime(int[] lst, int[] ld, int[] wst, int[] wd) {
+
+        int n = lst.length;
+        int m = wst.length;
+
+        int part1 = 0; //land then water
+
+        int min1 = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            min1 = Math.min(min1, lst[i] + ld[i]);
+        }
+        int min2 = Integer.MAX_VALUE;
+        for(int i = 0; i < m; i++){
+            if(wst[i] <= min1){
+                min2 = Math.min(min2, wd[i]);
+            }else if(wst[i] > min1){
+                min2 = Math.min(min2, wst[i] - min1 + wd[i]);
+            }
+        }
+        if(min2 == Integer.MAX_VALUE) part1 = Integer.MAX_VALUE;
+        else part1 = min1 + min2;
+
+        int part2 = 0; // water then land
+
+        int min3 = Integer.MAX_VALUE;
+        for(int i = 0; i < m; i++){
+            min3 = Math.min(min3, wst[i] + wd[i]);
+        }
+        int min4 = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            if(lst[i] <= min3){
+                min4 = Math.min(min4, ld[i]);
+            }else if(lst[i] > min3){
+                min4 = Math.min(min4, lst[i] - min3 + ld[i]);
+            }
+        }
+        if(min4 == Integer.MAX_VALUE) part2 = Integer.MAX_VALUE;
+        else part2 = min3 + min4;
+
+        return Math.min(part1, part2);
+    }
+}
