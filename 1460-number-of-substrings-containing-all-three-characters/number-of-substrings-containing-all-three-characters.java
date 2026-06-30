c6b7195dc7decaf3fe797946f1c705bfd173920e
Priyanshu Bokde <priyanshubokde08@gmail.com>
2026-06-30 07:50:29 +0530

    Time: 9 ms (97.93%) | Memory: 46 MB (90.39%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,34 +1,19 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int count = 0;int n = s.length();
-        int f[] = new int[3];
-        int l = 0;
-        for(int r = 0; r < n; r++){
-            f[s.charAt(r) - 'a']++;
-            while(f[0] > 0 && f[1] > 0 && f[2] > 0){
-                count += n - r;
-                f[s.charAt(l) - 'a']--;
-                l++;
+        int a = -1,b=-1,c=-1;
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == 'a'){
+                a = i;
+            }
+            else if(ch == 'b') b = i;
+            else c = i;
+            int min = Math.min(a,Math.min(b,c));
+            if(min!=-1){
+                count+=min+1;
             }
         }
-        
-    // for(int i = 0; i <= n-3; i++){
-//     int a = 0;int b = 0; int c = 0;int k = 0;
-//     while(k < 3){
-//         if(s.charAt(i + k) == 'a') a++;
-//         else if(s.charAt(i + k) == 'b') b++;
-//         else if(s.charAt(i + k) == 'c') c++;
-//         k++;
-//     }
-//     if(a > 0 && b > 0 && c > 0) count++;
-//     for(int j = i + 3; j < n; j++){
-//         if(s.charAt(j) == 'a') a++;
-//         else if(s.charAt(j) == 'b') b++;
-//         else if(s.charAt(j) == 'c') c++;
-//         if(a > 0 && b > 0 && c > 0) count++;
-//     }
-// }
-
         return count;
     }
 }
