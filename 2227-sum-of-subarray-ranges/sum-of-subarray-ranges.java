6dbce4bb0e1b3f5959aebfff7c94675af2bf2995
Priyanshu Bokde <priyanshubokde08@gmail.com>
2026-07-17 23:35:22 +0530

    Time: 30 ms (32.70%) | Memory: 46.9 MB (22.48%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,16 +1,53 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-        long sum = 0;
-        for(int i = 0; i < n; i++){
-            long max = Integer.MIN_VALUE;
-            long min = Integer.MAX_VALUE;
-            for(int j = i; j < n; j++){
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
-                sum += (max - min);
-            }
+    static long SumOfmaxOfSubArray(int[] arr){
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int nge[] = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && arr[st.peek()] < arr[i]) st.pop();
+            nge[i] = st.empty() ? n : st.peek();
+            st.push(i);
         }
-        return sum;
+        while(!st.empty()) st.pop();
+        int pge[] = new int[n];
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.peek()] <= arr[i]) st.pop();
+            pge[i] = st.empty() ? -1 : st.peek();
+            st.push(i);
+        }
+        long total = 0;
+        for(int i = 0; i < n; i++){
+            int left = i - pge[i];
+            int right = nge[i] - i;
+            total = total + ((long)left*right*arr[i]);
+        }
+        return total;
+    }
+    static long SumOfMinOfSubArray(int[] arr){
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int nse[] = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && arr[st.peek()] >= arr[i]) st.pop();
+            nse[i] = st.empty() ? n : st.peek();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        int pse[] = new int[n];
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.peek()] > arr[i]) st.pop();
+            pse[i] = st.empty() ? -1 : st.peek();
+            st.push(i);
+        }
+        long total = 0;
+        for(int i = 0; i < n; i++){
+            int left = i - pse[i];
+            int right = nse[i] - i;
+            total = total + ((long)left*right*arr[i]);
+        }
+        return total;
+    }
+    public long subArrayRanges(int[] nums) {
+        return SumOfmaxOfSubArray(nums) - SumOfMinOfSubArray(nums);
     }
 }
